bcf89c48b1f138e3d8ab9149c45aef2ad47e05be
unknown <kalpesh@evoluzn.in>
2023-09-12 19:46:00 +0530

    worked on matplotlib and files

--- flask/matplot_reka/stack.py
+++ flask/matplot_reka/stack.py
@@ -2,17 +2,17 @@ import matplotlib.pyplot as plt
 
 days = [1,2,3,4,5]
 
-sleeping = [6,4,7,8,5]
-eating = [9,5,4,1,0]
-plying = [9,5,7,3,2]
-working = [8,5,3,9,4]
+sleeping = [5,8,4,2,6]
+eating = [7,1,0,2,5]
+plying = [6,9,2,3,4]
+working = [1,0,9,5,7]
 
-plt.plot([],[],color='m',label = 'sleeping', linewidth=5)
-plt.plot([],[],color='c',label = 'Eating', linewidth=5)
-plt.plot([],[],color='r',label = 'Working', linewidth=5)
-plt.plot([],[],color='k',label = 'Playing', linewidth=5)
+plt.plot([],[],color='m',label = 'sleeping', linewidth=1)
+plt.plot([],[],color='c',label = 'Eating', linewidth=1)
+plt.plot([],[],color='r',label = 'Working', linewidth=1)
+plt.plot([],[],color='k',label = 'Playing', linewidth=1)
 
-plt.stackplot(days,sleeping,eating,working,plying,working, colors=['m','c','r','k'])
+plt.stackplot(days,sleeping,eating,working,plying, colors=['m','c','r','k'])
 
 plt.xlabel('x')
 plt.ylabel('y')
