3332ce2ff62ee44b423f611688a09d901a82ca0a
unknown <kalpesh@evoluzn.in>
2023-10-11 16:22:06 +0530

    we can define two route for same function

--- python/flask/app.py
+++ python/flask/app.py
@@ -6,7 +6,12 @@ def hello():
     msg = "Hello Word!!!!1"
     return msg
 
+@HelloApp.route('/blog')
+@HelloApp.route('/blog/<int:blog_no>')
+def blog(blog_no=0):
+    return 'This is blog number : '+ str(blog_no)
+
 HelloApp.add_url_rule('/hello','hello',hello)
-# print('Flask(__name__) : ',__name__)
-# if __name__ == '__main__':
-#     HelloApp.run(host='0.0.0.0',port=5001, debug=False)
+print('Flask(__name__) : ',__name__)
+if __name__ == '__main__':
+    HelloApp.run(host='0.0.0.0',port=5001, debug=False)
