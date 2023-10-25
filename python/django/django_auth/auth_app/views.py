93e3ca5d015b95e7407d5b3df97d9ea8852df153
kalpesh <kalpesh4111993@gmail.com>
2023-10-25 12:24:31 +0530

    used javascript in  django

--- python/django/django_auth/auth_app/views.py
+++ python/django/django_auth/auth_app/views.py
@@ -43,6 +43,10 @@ def UserLogin(request):
             request.session['Firstname'] = user.Firstname
             request.session['Lastname'] = user.Lastname
             request.session['Password'] = user.Password
+            request.session['ImageName'] = user.ImageName
+            request.session['pic'] = 'MEDIA_URL/'+ str(user.pic) 
+            # request.session['pic'] = str(user.pic)
+            print("  request.session['pic'] >>>>> ",  request.session['pic'])
             return render(request, 'auth_app/home.html')
         else:
             message = "Password doest Not Exist"
--- python/django/django_auth/authentication/settings.py
+++ python/django/django_auth/authentication/settings.py
@@ -123,7 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+import os
+
 STATIC_URL = 'static/'
+# Base url to serve media files
+MEDIA_URL = '/media/'
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
